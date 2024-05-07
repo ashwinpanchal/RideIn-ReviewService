a5eafa0daa4799a3ba86d1575559d2d039d4145d
ashwinpanchal <ashwinpanchal.iitb@gmail.com>
2024-05-07 23:31:09 +0530

    Basic setup and configuration setup for database

---
+++ src/main/java/org/example/rideinreviewservice/RideInReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.rideinreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideInReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideInReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rideinreviewservice/RideInReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rideinreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideInReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
